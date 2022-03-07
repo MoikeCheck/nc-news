c985ca9cf5beae5e307235a3f0bc0160d562252f
MoikeCheck <mikewheatley@hotmail.co.uk>
2022-03-07 14:18:35 +0000

    view list of all articles ticket started

---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,9 @@
+export default function ArticleCard({ ArticleName }) {
+  return (
+    <article className="article__card">
+      <h3>{ArticleName}</h3>
+    </article>
+  );
+}
+
+
---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import ArticleCard from "./ArticleCard";
+import * as api from "../Api";
+
+export default function PlanetList() {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    api.getArticles().then((articles) => {
+      setArticles(articles);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) return <p>loading..</p>;
+  return (
+    <section className="section__article">
+      {articles.map(({ title }) => {
+        return <ArticleCard ArticleName={title} />;
+      })}
+    </section>
+  );
+}
