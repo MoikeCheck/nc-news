2dfdc219f1961ae58e23b930c6958ed28878a135
MoikeCheck <mikewheatley@hotmail.co.uk>
2022-04-13 12:06:54 +0100

    connected my own api

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const myApi = axios.create({
-  baseURL: "https://nc-news-example-seminar-2-3.herokuapp.com/api",
+  baseURL: "https://mlw-news.herokuapp.com/api",
 });
 
 export const getArticles = (slug, sort_by, order) => {
